2f8b081082ee474bfff84b8839b1884a118ee2be
joelcaminas02 <joelcg2002@gmail.com>
2022-02-21 09:26:49 +0100

    codigo refactorizado

--- capitulo4/src/com/company/Main.java
+++ capitulo4/src/com/company/Main.java
@@ -6,15 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
-        int dato=teclado.nextInt();
-        int vector[]=new int[dato];
-        System.out.println("\nVector inicial hasta :"+dato);
+        int numero=teclado.nextInt();
+        int vector[]=new int[numero];
+        System.out.println("\nVector inicial hasta :"+numero);
         for (int i = 0; i < vector.length; i++) {
             if (i%10==0) System.out.println();
             System.out.print(i+1+"\t");
         }
-        vector=generarPrimos(dato);
-        System.out.println("\nVector de primos hasta:"+dato);
+        vector=generarPrimos(numero);
+        System.out.println("\nVector de primos hasta:"+numero);
         for (int i = 0; i < vector.length; i++) {
             if (i%10==0) System.out.println();
             System.out.print(vector[i]+"\t");
